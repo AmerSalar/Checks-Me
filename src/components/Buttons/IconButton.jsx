64990efeaf19d5290ad771967cd2b1d5c4cb74fe
Ameer Salar <amersalar108@gmail.com>
2026-02-12 21:17:09 +0300

    Edited navbar UI

--- src/components/Buttons/IconButton.jsx
+++ src/components/Buttons/IconButton.jsx
@@ -1,15 +1,29 @@
 import styles from "./Buttons.module.css";
 import {} from "react";
+import clsx from "clsx";
 
-function IconButton({ icon, set = null, func = null }) {
+function IconButton({
+  icon,
+  set = null,
+  func = null,
+  active = false,
+  toggle,
+  i = null,
+}) {
   function handle(e) {
+    if (toggle) toggle(i);
+
     if (func != null) func(e);
+
     if (set) {
       set();
     }
   }
   return (
-    <button className={styles.icon} onClick={(e) => handle(e)}>
+    <button
+      className={clsx(styles.icon, active && styles.on, !active && styles.off)}
+      onClick={(e) => handle(e)}
+    >
       {icon}
     </button>
   );
--- src/components/HeaderFooter/Footer.jsx
+++ src/components/HeaderFooter/Footer.jsx
@@ -7,19 +7,47 @@ import {
   FiCalendar,
   FiPieChart,
 } from "react-icons/fi";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Context from "../App/Context";
 function Footer() {
+  const [selected, setSelected] = useState(0);
   const { set } = useContext(Context);
+  function handleToggle(i) {
+    setSelected(i);
+  }
   return (
     <div className={styles.cont}>
-      <IconButton icon={<FiList color="rgb(100, 175, 245)" />} />
-      <IconButton icon={<FiCalendar />} />
+      <div className={styles.sideCont}>
+        <IconButton
+          icon={<FiList />}
+          i={0}
+          active={selected === 0}
+          toggle={handleToggle}
+        />
+        <IconButton
+          icon={<FiCalendar />}
+          i={1}
+          active={selected === 1}
+          toggle={handleToggle}
+        />
+      </div>
       <div className={styles.plusCont}>
         <IconButton icon={<FiPlusCircle size="45px" />} set={set} />
       </div>
-      <IconButton icon={<FiPieChart />} />
-      <IconButton icon={<FiUser />} />
+      <div className={styles.sideCont}>
+        <IconButton
+          icon={<FiPieChart />}
+          i={2}
+          active={selected === 2}
+          toggle={handleToggle}
+        />
+        <IconButton
+          icon={<FiUser />}
+          i={3}
+          active={selected === 3}
+          toggle={handleToggle}
+        />
+      </div>
     </div>
   );
 }
